221908e21011f9de4963eef5a1a9cf2923ac1545
kartikkashyap10 <kykashyap10@gmail.com>
2024-06-08 18:34:52 +0530

    episode 3 part 4

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement(
-    "h1",
-    { id: "heading" },
-    "Welcome to Namaste React🚀"
+  "h1",
+  { id: "heading" },
+  "Welcome to Namaste React🚀"
 );
 
 console.log(heading);
 
-const jsxHeading = <h1 id="heading">Bonjour to Namaste React🚀</h1>;
+// JSX Element
+const jsxHeading = (
+  <h1 id="heading" className="heading">
+    Bonjour to Namaste React (JSX)🚀
+  </h1>
+);
 console.log(jsxHeading);
 
+// React functional component : a function returning jsx
+// Babel transpiles the jsx code into valid react js code to create an element
+const HeadingComponent = () => {
+  // return keyword is optional
+  return (
+    <div>
+      {/* component composition */}
+      <Title />
+      <h1 className="heading">Heading Functional Component</h1>
+    </div>
+  );
+};
+
+const Title = () => (
+  <h1 className="title">Title Functional Component</h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
